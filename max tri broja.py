c0a031a4d5f2ad702a25afb9ca61aead45994007
TihoM1 <73365957+TihoM1@users.noreply.github.com>
2020-10-27 23:44:26 +0100

    Add files via upload

---
+++ max tri broja.py
@@ -0,0 +1,11 @@
+"""
+Kod koji od tri broja x,y,z vraca najveci.
+"""
+def najveci_od_tri_broja(tri_broja):
+    print(max(tri_broja))
+
+X=int(input("x = "))
+Y=int(input("y = "))
+Z=int(input("z = "))
+niz_tri_broja=[X,Y,Z]
+najveci_od_tri_broja(niz_tri_broja)
